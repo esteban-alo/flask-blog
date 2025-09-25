dcbc49e62fe5d71179e8cf9a1aec75e250bf0e0a
esteban-alo <esrodriguezalo@gmail.com>
2025-09-25 16:34:40 -0500

    test: added test_env_not_found

--- tests/unit/settings/test_config.py
+++ tests/unit/settings/test_config.py
@@ -2,6 +2,8 @@
 Config class tests cases
 """
 
+import pytest
+
 from src.settings.config import Settings, get_env_path
 
 
@@ -10,6 +12,16 @@ def test_get_env_path():
     Test get_env_path finds .env file
     """
     assert get_env_path() is not None
+
+
+def test_env_not_found(tmp_path, monkeypatch):
+    """
+    Use pytest’s tmp_path fixture together with monkeypatch to change the working directory and verify that an exception is raised when the .env file is not found
+    """
+    monkeypatch.chdir(tmp_path)
+
+    with pytest.raises(FileNotFoundError, match=".env file not found"):
+        get_env_path()
 
 
 def test_load_settings_class():
